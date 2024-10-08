5f8020355ffc9043cf4b26c2745980176f6777a8
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-10-08 07:50:29 +0530

    Update tg.js

--- tg.js
+++ tg.js
@@ -1,10 +1,11 @@
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.Token);
+var helpText='🔰This is an Mega.nz File downloader advance bot!💪😊\n  Send Me a mega link to see the magic\n  And I am authers only bot😉\n\n🔰Commands:\n  /help -for help\n  /stop -for stop file sending!\n  /run0 -to clear dl and set run to 0 and stop runing \n  /ts -to resume sending if error or stoped unfortunatedly\n  /skipF -skip current file if only erroring!\n  /sk--index -to skip some bulk of file stop sending before use!\n  /rerun -to start the stoped by /stop\n  /isf -check the download path\n  /numF -check the reiming file count\n\nGood Day!😇';
 
 bot.start((ctx) => ctx.reply('Welcome! 👋 I am a personal mega downloader bot!😇'));
 
-bot.help((ctx) => ctx.reply('Use /start to start me, /help to see this message, or type something to talk to me!'));
+bot.help((ctx) => ctx.reply(helpText));
 
 
 bot.launch();
